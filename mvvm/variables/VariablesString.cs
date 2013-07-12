29bedbcfa142329876bed97e69af8eedc06a626f
Przemyslaw Madej <przemyslaw.madej@yahoo.pl>
2013-07-12 21:50:52 +0200

    poprawka tekstu o autorze

--- mvvm/variables/VariablesString.cs
+++ mvvm/variables/VariablesString.cs
@@ -11,7 +11,7 @@ namespace mvvm.variables
         public static readonly string StartText = "Rozpocznij zabawe";
         public static readonly string MapKreator = "Kreator Map";
         public static readonly string AboutAuthor = "O autorze";
-        public static readonly string AboutAuthorText = "Aplikacja napisana przez Przemysława Madeja";
+        public static readonly string AboutAuthorText = "Aplikacja napisana przez studenta Informatyki wydziału Elektroniki i technik Informacyjnych. W ramach pracy inzynierkiej Przemysława Madeja";
         public static readonly string KinectConfigurationText = "Wszystkie urzadzenia podlaczone do komuputera zostala zainicjalizowane. Wybierz urzadzenie z listy nastepne klinij przycisk \" Wskaz Sensor \" ";
     }
 }
